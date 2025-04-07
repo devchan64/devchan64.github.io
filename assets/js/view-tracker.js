ceeb198e7437b7b4bb94782b867011d2201855bd
cbsim <cbsim@brownbag.one>
2025-04-08 08:48:47 +0900

    slug 체크

--- assets/js/view-tracker.js
+++ assets/js/view-tracker.js
@@ -1,9 +1,13 @@
 (async () => {
-  await fetch("https://dxwpncxfauzspnqwwbxj.supabase.co/functions/v1/safe-increment-view", {
+  const slug = location.pathname;
+  console.log(slug)
+  await fetch("https://<your-project>.functions.supabase.co/safe-increment-view", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ slug: location.pathname })
-  });  
+    body: JSON.stringify({
+      slug: slug
+    })
+  });
 })();
