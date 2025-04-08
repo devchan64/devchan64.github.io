4cb03324552c45de20cb333a926859a5d74dfdb2
cbsim <cbsim@brownbag.one>
2025-04-08 09:22:28 +0900

    콤마 누락

--- assets/js/view-tracker.js
+++ assets/js/view-tracker.js
@@ -4,7 +4,7 @@
   await fetch("https://dxwpncxfauzspnqwwbxj.supabase.co/functions/v1/safe-increment-view", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4d3BuY3hmYXV6c3BucXd3YnhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwNDE2MjQsImV4cCI6MjA1OTYxNzYyNH0.1Pdrk8G9UjzTBdE5w__5led6LjeXIdN_8g5mDLIQnIg"
     },
     body: JSON.stringify({
